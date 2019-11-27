97695ef85b7c3d65da176643782f664f73d149da
missmeow71 <sensuousdesigner@gmail.com>
2019-11-27 16:26:57 -0700

    fixing contact form

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@ $(() => {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
-emailjs
-window.onload = () => {
-    document.querySelector('#contact-form').addEventListener('submit', (event) => {
-        event.preventDefault()
-        emailjs.send("smtp_server", "template_sONPlQTz", {})
-            .then((res) => {
-                console.log("SUCCESS. status=%d, text=%s", res.status, res.text)
-            }, (err) => {
-                console.log("FAILED. error=", err)
-            })
-    })
-}
+// emailjs
+// window.onload = () => {
+//     document.querySelector('#contact-form').addEventListener('submit', (event) => {
+//         event.preventDefault()
+//         emailjs.send("smtp_server", "template_sONPlQTz", {})
+//             .then((res) => {
+//                 console.log("SUCCESS. status=%d, text=%s", res.status, res.text)
+//             }, (err) => {
+//                 console.log("FAILED. error=", err)
+//             })
+//     })
+// }
