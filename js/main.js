41b9cabef8ebc3ddc1fd7a522111ec36157f114e
missmeow71 <sensuousdesigner@gmail.com>
2019-11-26 20:50:25 -0700

    changing contact form

--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@ $(() => {
 })
 
 //emailjs
-window.onload = () => {
-    document.querySelector('#contact-form').addEventListener('submit', (event) => {
-        event.preventDefault()
-        emailjs.send("smtp_server", "template_sONPlQTz", {})
-            .then((res) => {
-                console.log("SUCCESS. status=%d, text=%s", res.status, res.text)
-            }, (err) => {
-                console.log("FAILED. error=", err)
-            })
-    })
-}
+// window.onload = () => {
+//     document.querySelector('#contact-form').addEventListener('submit', (event) => {
+//         event.preventDefault()
+//         emailjs.send("smtp_server", "template_sONPlQTz", {})
+//             .then((res) => {
+//                 console.log("SUCCESS. status=%d, text=%s", res.status, res.text)
+//             }, (err) => {
+//                 console.log("FAILED. error=", err)
+//             })
+//     })
+// }
