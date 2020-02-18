c477dc0383fb2f8b67f4ca95a53a758c4c644a8b
missmeow71 <sensuousdesigner@gmail.com>
2020-02-18 15:06:35 -0700

    removed contact form & updated about me

--- js/main.js
+++ js/main.js
@@ -5,23 +5,23 @@ $(() => {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
-// emailjs
- window.onload = function () {
-       document.getElementById('contact-form').addEventListener('submit', function (event) {
-           event.preventDefault();
-           // generate the contact number value
-           this.contact_number.value = Math.random() * 100000 | 0;
-           emailjs.sendForm('contact_service', 'contact_template', this);
-       });
-   }
+// // emailjs
+//  window.onload = function () {
+//        document.getElementById('contact-form').addEventListener('submit', function (event) {
+//            event.preventDefault();
+//            // generate the contact number value
+//            this.contact_number.value = Math.random() * 100000 | 0;
+//            emailjs.sendForm('contact_service', 'contact_template', this);
+//        });
+//    }
 
-var template_params = {
-    "reply_to": "reply_to_value",
-    "from_name": "from_name_value",
-    "Sandy": "Sandy_value",
-    "message_html": "message_html_value"
-}
+// var template_params = {
+//     "reply_to": "reply_to_value",
+//     "from_name": "from_name_value",
+//     "Sandy": "Sandy_value",
+//     "message_html": "message_html_value"
+// }
 
-var service_id = "default_service";
-var template_id = "template_5tbT5JWJ";
-emailjs.send(service_id, template_id, template_params);
+// var service_id = "default_service";
+// var template_id = "template_5tbT5JWJ";
+// emailjs.send(service_id, template_id, template_params);
